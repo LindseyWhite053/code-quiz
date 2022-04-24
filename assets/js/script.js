0730b8aa5825e44bb246eff0eb96016d5bbeae9a
Lindsey White <LindseyWhite053@Gmail.com>
2022-04-24 03:10:24 -0400

    questions and answer buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var timerEl = document.querySelector('#timer');
 var mainEl = document.querySelector('main');
+var timeLeft = 75;
 
 var pageContentEl = document.createElement("div");
 pageContentEl.id = "container";
@@ -12,12 +13,39 @@ startButtonEl.className = "answer-button";
 startButtonEl.innerHTML = "Start Quiz";
 pageContentEl.appendChild(startButtonEl);
 
+var answerOneEl = document.createElement("button");
+answerOneEl.id = "choice-one";
+answerOneEl.className = "answer-button";
+answerOneEl.innerHTML = "";
+
+var answerTwoEl = document.createElement("button");
+answerTwoEl.id = "choice-two";
+answerTwoEl.className = "answer-button";
+answerTwoEl.innerHTML = "";
+
+var answerThreeEl = document.createElement("button");
+answerThreeEl.id = "choice-three";
+answerThreeEl.className = "answer-button";
+answerThreeEl.innerHTML = "";
+
+var answerFourEl = document.createElement("button");
+answerFourEl.id = "choice-four";
+answerFourEl.className = "answer-button";
+answerFourEl.innerHTML = "";
+
+var confirmationEl = document.createElement("p");
+confirmationEl.id = "confirmation";
+confirmationEl.innerHTML = ""
+
+
 var startQuizEl = document.querySelector('#start-button');
+
+
 
 function startQuiz() {
 questionOne();
 var startQuizEl = document.querySelector('#start-button');
-var timeLeft = 75;
+
 
 timerEl.textContent = "Timer: " + timeLeft;
 
@@ -37,71 +65,314 @@ timerEl.textContent = "Timer: " + timeLeft;
 function questionOne(){
 pageContentEl.innerHTML = "<h1>Commonly used data types do NOT include?</h1>"
 
-var answerOneEl = document.createElement("button");
-answerOneEl.id = "choice-one";
-answerOneEl.className = "answer-button";
+
 answerOneEl.innerHTML = "strings";
 pageContentEl.appendChild(answerOneEl);
 
-var answerTwoEl = document.createElement("button");
-answerTwoEl.id = "choice-two";
-answerTwoEl.className = "answer-button";
+
 answerTwoEl.innerHTML = "booleans";
 pageContentEl.appendChild(answerTwoEl);
 
-var answerThreeEl = document.createElement("button");
-answerThreeEl.id = "choice-three";
-answerThreeEl.className = "answer-button";
+
 answerThreeEl.innerHTML = "alerts";
 pageContentEl.appendChild(answerThreeEl);
 
-var answerFourEl = document.createElement("button");
-answerFourEl.id = "choice-four";
-answerFourEl.className = "answer-button";
+
 answerFourEl.innerHTML = "numbers";
 pageContentEl.appendChild(answerFourEl);
 
 var choiceOneEl = document.querySelector("#choice-one");
-choiceOneEl.addEventListener("click", questionTwoIncorrect);
+choiceOneEl.addEventListener("click", questionOneIncorrect);
 
 var choiceTwoEl = document.querySelector("#choice-two");
-choiceTwoEl.addEventListener("click", questionTwoIncorrect);
+choiceTwoEl.addEventListener("click", questionOneIncorrect);
 
 var choiceThreeEl = document.querySelector("#choice-three");
-choiceThreeEl.addEventListener("click", questionTwoCorrect);
+choiceThreeEl.addEventListener("click", questionOneCorrect);
 
 var choiceFourEl = document.querySelector("#choice-four");
-choiceFourEl.addEventListener("click", questionTwoIncorrect);
+choiceFourEl.addEventListener("click", questionOneIncorrect);
 
 };
 
-function questionTwoIncorrect() {
-    // timeLeft -= 10;
+function questionOneIncorrect() {
+    timeLeft -= 10;
     pageContentEl.innerHTML = "<h1>The condition in an if/else  statement is enclosed with _____. </h1>";
-    // answerOneEl.innerHTML = "quotes";
-    // answerTwoEl.innerHTML = "curly brackets";
-    // answerThreeEl.innerHTML = "parenthesis";
-    // answerFourEl.innerHTML = "square brackets";
+    answerOneEl.innerHTML = "quotes";
+    answerTwoEl.innerHTML = "curly brackets";
+    answerThreeEl.innerHTML = "parenthesis";
+    answerFourEl.innerHTML = "square brackets";
 
-    var confirmationEl = document.createElement("p");
-    confirmationEl.id = "confirmation";
-    confirmationEl.innerHTML = "Incorrect answer provided"
+    pageContentEl.appendChild(answerOneEl);
+    pageContentEl.appendChild(answerTwoEl);
+    pageContentEl.appendChild(answerThreeEl);
+    pageContentEl.appendChild(answerFourEl);
+
+
+    confirmationEl.innerHTML = "Incorrect answer provided."
     pageContentEl.appendChild(confirmationEl);
+
+    var choiceOneEl = document.querySelector("#choice-one");
+    choiceOneEl.addEventListener("click", questionTwoIncorrect);
+
+    var choiceTwoEl = document.querySelector("#choice-two");
+    choiceTwoEl.addEventListener("click", questionTwoCorrect);
+
+    var choiceThreeEl = document.querySelector("#choice-three");
+    choiceThreeEl.addEventListener("click", questionTwoIncorrect);
+
+    var choiceFourEl = document.querySelector("#choice-four");
+    choiceFourEl.addEventListener("click", questionTwoIncorrect);
+
+}
+
+function questionOneCorrect() {
+    pageContentEl.innerHTML = "<h1>The condition in an if/else  statement is enclosed with _____. </h1>";
+    answerOneEl.innerHTML = "quotes";
+    answerTwoEl.innerHTML = "curly brackets";
+    answerThreeEl.innerHTML = "parenthesis";
+    answerFourEl.innerHTML = "square brackets";
+
+    pageContentEl.appendChild(answerOneEl);
+    pageContentEl.appendChild(answerTwoEl);
+    pageContentEl.appendChild(answerThreeEl);
+    pageContentEl.appendChild(answerFourEl);
+
+    confirmationEl.innerHTML = "Correct answer!"
+    pageContentEl.appendChild(confirmationEl);
+
+    var choiceOneEl = document.querySelector("#choice-one");
+    choiceOneEl.addEventListener("click", questionTwoIncorrect);
+
+    var choiceTwoEl = document.querySelector("#choice-two");
+    choiceTwoEl.addEventListener("click", questionTwoCorrect);
+
+    var choiceThreeEl = document.querySelector("#choice-three");
+    choiceThreeEl.addEventListener("click", questionTwoIncorrect);
+
+    var choiceFourEl = document.querySelector("#choice-four");
+    choiceFourEl.addEventListener("click", questionTwoIncorrect);
+}
+
+function questionTwoIncorrect() {
+    timeLeft -= 10;
+    pageContentEl.innerHTML = "<h1>Arrays in JavaScript can be used to store ___ . </h1>";
+    answerOneEl.innerHTML = "numbers and strings";
+    answerTwoEl.innerHTML = "other arrays";
+    answerThreeEl.innerHTML = "booleans";
+    answerFourEl.innerHTML = "all of the above";
+
+    pageContentEl.appendChild(answerOneEl);
+    pageContentEl.appendChild(answerTwoEl);
+    pageContentEl.appendChild(answerThreeEl);
+    pageContentEl.appendChild(answerFourEl);
+
+
+    confirmationEl.innerHTML = "Incorrect answer provided."
+    pageContentEl.appendChild(confirmationEl);
+
+    var choiceOneEl = document.querySelector("#choice-one");
+    choiceOneEl.addEventListener("click", questionThreeIncorrect);
+
+    var choiceTwoEl = document.querySelector("#choice-two");
+    choiceTwoEl.addEventListener("click", questionThreeIncorrect);
+
+    var choiceThreeEl = document.querySelector("#choice-three");
+    choiceThreeEl.addEventListener("click", questionThreeIncorrect);
+
+    var choiceFourEl = document.querySelector("#choice-four");
+    choiceFourEl.addEventListener("click", questionThreeCorrect);
 
 }
 
 function questionTwoCorrect() {
-    pageContentEl.innerHTML = "<h1>The condition in an if/else  statement is enclosed with _____. </h1>";
-    // answerOneEl.innerHTML = "quotes";
-    // answerTwoEl.innerHTML = "curly brackets";
-    // answerThreeEl.innerHTML = "parenthesis";
-    // answerFourEl.innerHTML = "square brackets";
+    pageContentEl.innerHTML = "<h1>Arrays in JavaScript can be used to store ___ . </h1>";
+    answerOneEl.innerHTML = "numbers and strings";
+    answerTwoEl.innerHTML = "other arrays";
+    answerThreeEl.innerHTML = "booleans";
+    answerFourEl.innerHTML = "all of the above";
 
-    var confirmationEl = document.createElement("p");
-    confirmationEl.id = "confirmation";
-    confirmationEl.innerHTML = "Correct answer!";
+    pageContentEl.appendChild(answerOneEl);
+    pageContentEl.appendChild(answerTwoEl);
+    pageContentEl.appendChild(answerThreeEl);
+    pageContentEl.appendChild(answerFourEl);
+
+    confirmationEl.innerHTML = "Correct answer!"
     pageContentEl.appendChild(confirmationEl);
+
+    var choiceOneEl = document.querySelector("#choice-one");
+    choiceOneEl.addEventListener("click", questionThreeIncorrect);
+
+    var choiceTwoEl = document.querySelector("#choice-two");
+    choiceTwoEl.addEventListener("click", questionThreeIncorrect);
+
+    var choiceThreeEl = document.querySelector("#choice-three");
+    choiceThreeEl.addEventListener("click", questionThreeIncorrect);
+
+    var choiceFourEl = document.querySelector("#choice-four");
+    choiceFourEl.addEventListener("click", questionThreeCorrect);
 }
+
+function questionThreeIncorrect() {
+    timeLeft -= 10;
+    pageContentEl.innerHTML = "<h1>String values must be enclosed within _____ when being assigned variables. </h1>";
+    answerOneEl.innerHTML = "commas";
+    answerTwoEl.innerHTML = "curly braces";
+    answerThreeEl.innerHTML = "quotes";
+    answerFourEl.innerHTML = "parenthesis";
+
+    pageContentEl.appendChild(answerOneEl);
+    pageContentEl.appendChild(answerTwoEl);
+    pageContentEl.appendChild(answerThreeEl);
+    pageContentEl.appendChild(answerFourEl);
+
+
+    confirmationEl.innerHTML = "Incorrect answer provided."
+    pageContentEl.appendChild(confirmationEl);
+
+    var choiceOneEl = document.querySelector("#choice-one");
+    choiceOneEl.addEventListener("click", questionFourIncorrect);
+
+    var choiceTwoEl = document.querySelector("#choice-two");
+    choiceTwoEl.addEventListener("click", questionFourIncorrect);
+
+    var choiceThreeEl = document.querySelector("#choice-three");
+    choiceThreeEl.addEventListener("click", questionFourCorrect);
+
+    var choiceFourEl = document.querySelector("#choice-four");
+    choiceFourEl.addEventListener("click", questionFourIncorrect);
+
+}
+
+function questionThreeCorrect() {
+    pageContentEl.innerHTML = "<h1>	String values must be enclosed within _____ when being assigned variables. </h1>";
+    answerOneEl.innerHTML = "commas";
+    answerTwoEl.innerHTML = "curly braces";
+    answerThreeEl.innerHTML = "quotes";
+    answerFourEl.innerHTML = "parenthesis";
+
+    pageContentEl.appendChild(answerOneEl);
+    pageContentEl.appendChild(answerTwoEl);
+    pageContentEl.appendChild(answerThreeEl);
+    pageContentEl.appendChild(answerFourEl);
+
+    confirmationEl.innerHTML = "Correct answer!"
+    pageContentEl.appendChild(confirmationEl);
+
+    var choiceOneEl = document.querySelector("#choice-one");
+    choiceOneEl.addEventListener("click", questionFourIncorrect);
+
+    var choiceTwoEl = document.querySelector("#choice-two");
+    choiceTwoEl.addEventListener("click", questionFourIncorrect);
+
+    var choiceThreeEl = document.querySelector("#choice-three");
+    choiceThreeEl.addEventListener("click", questionFourCorrect);
+
+    var choiceFourEl = document.querySelector("#choice-four");
+    choiceFourEl.addEventListener("click", questionFourIncorrect);
+}
+
+function questionFourIncorrect() {
+    timeLeft -= 10;
+    pageContentEl.innerHTML = "<h1>	A very useful tool used during development and debugging for printing content to the debugger is: </h1>";
+    answerOneEl.innerHTML = "JavaScript";
+    answerTwoEl.innerHTML = "Terminal/bash";
+    answerThreeEl.innerHTML = "for loops";
+    answerFourEl.innerHTML = "console.log";
+
+    pageContentEl.appendChild(answerOneEl);
+    pageContentEl.appendChild(answerTwoEl);
+    pageContentEl.appendChild(answerThreeEl);
+    pageContentEl.appendChild(answerFourEl);
+
+
+    confirmationEl.innerHTML = "Incorrect answer provided."
+    pageContentEl.appendChild(confirmationEl);
+
+    var choiceOneEl = document.querySelector("#choice-one");
+    choiceOneEl.addEventListener("click", questionFourIncorrect);
+
+    var choiceTwoEl = document.querySelector("#choice-two");
+    choiceTwoEl.addEventListener("click", questionFourIncorrect);
+
+    var choiceThreeEl = document.querySelector("#choice-three");
+    choiceThreeEl.addEventListener("click", questionFourCorrect);
+
+    var choiceFourEl = document.querySelector("#choice-four");
+    choiceFourEl.addEventListener("click", questionFourIncorrect);
+
+}
+
+function questionFourCorrect() {
+    pageContentEl.innerHTML = "<h1>	A very useful tool used during development and debugging for printing content to the debugger is: </h1>";
+    answerOneEl.innerHTML = "JavaScript";
+    answerTwoEl.innerHTML = "Terminal/bash";
+    answerThreeEl.innerHTML = "for loops";
+    answerFourEl.innerHTML = "console.log";
+
+    pageContentEl.appendChild(answerOneEl);
+    pageContentEl.appendChild(answerTwoEl);
+    pageContentEl.appendChild(answerThreeEl);
+    pageContentEl.appendChild(answerFourEl);
+
+    confirmationEl.innerHTML = "Correct answer!"
+    pageContentEl.appendChild(confirmationEl);
+
+    var choiceOneEl = document.querySelector("#choice-one");
+    choiceOneEl.addEventListener("click", questionFiveIncorrect);
+
+    var choiceTwoEl = document.querySelector("#choice-two");
+    choiceTwoEl.addEventListener("click", questionFiveIncorrect);
+
+    var choiceThreeEl = document.querySelector("#choice-three");
+    choiceThreeEl.addEventListener("click", questionFiveIncorrect);
+
+    var choiceFourEl = document.querySelector("#choice-four");
+    choiceFourEl.addEventListener("click", questionFiveCorrect);
+}
+
+function questionFiveIncorrect() {
+    timeLeft -= 10;
+    pageContentEl.innerHTML = "<h1>	Your final score is... </h1>";
+
+    confirmationEl.innerHTML = "Incorrect answer provided."
+    pageContentEl.appendChild(confirmationEl);
+
+}
+
+function questionFiveCorrect() {
+    confirmationEl.innerHTML = "Correct answer!"
+    pageContentEl.appendChild(confirmationEl);
     
+    pageContentEl.innerHTML = "<h1>	Your final score is... </h1>";
+
+    confirmationEl.innerHTML = "Correct answer!"
+    pageContentEl.appendChild(confirmationEl);
+
+}
+
+    
+// function highScoreSubmit () {
+// var score = timeLeft 
+// var highscore = localStorage.getItem("highscore");
+
+// if(!highscore) {
+
+//     if (score > highscore) {
+//         localStorage.setItem("highscore", score);
+//     } 
+// }
+// else {
+//     playAgain();
+// }
+// };
+
+// function highScoreRetrieve () {
+
+// }
+
+// function playAgain () {
+
+// }
 
 startQuizEl.addEventListener("click", startQuiz);
