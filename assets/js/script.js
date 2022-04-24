fd6081c80e5408e3838e19e8b8058e33d8596dd0
Lindsey White <LindseyWhite053@Gmail.com>
2022-04-23 23:44:43 -0400

    start page created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
 var timerEl = document.querySelector('#timer');
+var mainEl = document.querySelector('main');
+
+var pageContentEl = document.createElement("div");
+pageContentEl.id = "container";
+pageContentEl.innerHTML = "<h1>Coding Quiz Challenge</h1><p>Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize you score/time by ten seconds!</p>"
+mainEl.appendChild(pageContentEl);
+
+var startButtonEl = document.createElement("button");
+startButtonEl.id = "start-button";
+startButtonEl.innerHTML = "Start Quiz";
+pageContentEl.appendChild(startButtonEl);
+
 var startQuizEl = document.querySelector('#start-button');
 
-
 function startQuiz() {
-
+questionOne();
+var startQuizEl = document.querySelector('#start-button');
 var timeLeft = 75;
 
 timerEl.textContent = "Timer: " + timeLeft;
@@ -20,6 +32,10 @@ timerEl.textContent = "Timer: " + timeLeft;
     }
   }, 1000);
 };
+
+function questionOne(){
+
+}
 
 
 startQuizEl.addEventListener("click", startQuiz);
